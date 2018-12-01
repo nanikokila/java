2409fd49391abc1c08ee8f2e8a46fddbcc4015e0
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-01 16:29:59 +0530

    Collections

---
+++ CoreJavaExamples/src/main/java/com/app/ArrayListDemo.java
@@ -0,0 +1,30 @@
+package com.app;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> al = new ArrayList<String>();
+		System.out.println(al);
+		al.add("Ashok");
+		al.add("Naveen");
+		al.add("Naresh");
+		al.add("Vijay");
+		al.add("Ramya");
+		System.out.println(al);
+		System.out.println(al.size());
+		al.add("Sathish");
+		al.add("Kalyan");
+		al.remove(4);
+		System.out.println(al);
+		System.out.println(al.size());
+		al.add(4, "Ramya");
+		al.add("Ashok");
+		System.out.println(al);
+		System.out.println(al.size());
+		al.clear();
+	}
+
+}
