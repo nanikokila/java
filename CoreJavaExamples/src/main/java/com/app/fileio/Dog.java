5865a78b3e535d6a9e3cab2a9b7cf849f8940243
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-02 19:42:56 +0530

    serialzation

---
+++ CoreJavaExamples/src/main/java/com/app/fileio/Dog.java
@@ -0,0 +1,14 @@
+package com.app.fileio;
+
+import java.io.Serializable;
+
+public class Dog implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	int a=10;
+	int b=20;
+	
+}
---
+++ CoreJavaExamples/src/main/java/com/app/fileio/SeializationDemo.java
@@ -0,0 +1,25 @@
+package com.app.fileio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SeializationDemo {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+
+		Dog d1 = new Dog();
+		FileOutputStream fos = new FileOutputStream("E://java.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(d1);
+		
+		FileInputStream fis  = new FileInputStream("E://java.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		Dog d2 = (Dog)ois.readObject();
+		System.out.println(d1.a+" "+d2.b);
+	}
+
+}
