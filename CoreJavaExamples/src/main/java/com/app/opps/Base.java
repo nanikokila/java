bb19e444ed81c66d9aae2825b6ae00e145b92685
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-27 21:39:32 +0530

    static

---
+++ CoreJavaExamples/src/main/java/com/app/opps/Base.java
@@ -0,0 +1,28 @@
+package com.app.opps;
+
+public class Base {
+
+	static int i=10;
+
+	static 
+	{
+
+		methodOne();
+		System.out.println("first static block");
+	}
+	public static void main(String[] args) {
+
+		methodOne();
+		System.out.println("main method");
+
+	}
+	public static void methodOne()
+	{
+		System.out.println(j);
+	}
+	static 
+	{
+		System.out.println("second static block");
+	}
+	static int j=20;
+}
