77e9d286c9bf17943ac2ea63caeaf8af31d3395a
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-28 21:45:02 +0530

    sample

---
+++ CoreJavaExamples/src/main/java/com/app/strings/Sample.java
@@ -0,0 +1,25 @@
+package com.app.strings;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		String s1 = new String("you cannot change me");
+		String s2 = new String("you cannot change me");
+		System.out.println(s1==s2);//false
+		String s3 = "you cannot change me";
+		System.out.println(s1==s3);//false
+		String s4 = "you cannot change me";
+		System.out.println(s3==s4);//true
+		String s5 = "you cannot"+"change me";
+		System.out.println(s3==s5);//false
+		String s6 = "you cannot";
+		String s7 = s6+"change me";
+		System.out.println(s3==s7);//false
+		final String s8 = "you cannot";
+		String s9 = s8+"change me";
+		System.out.println(s3==s9);//false
+		System.out.println(s6==s8);//true
+	}
+
+}
