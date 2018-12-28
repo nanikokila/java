634ac2ae2d25478f5858814134a14e5ece6a8b98
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-28 21:25:31 +0530

    code added

--- CoreJavaExamples/src/main/java/com/app/strings/Demo.java
+++ CoreJavaExamples/src/main/java/com/app/strings/Demo.java
@@ -19,6 +19,24 @@ public class Demo {
 		StringBuffer sb = new StringBuffer("ashok");
 		sb.append("reddy");
 		System.out.println(sb);
+		
+		/*
+		 * In string class .equals() is overridden for content comparison and == operator
+		 * for reference comparison
+		 */
+		
+		String str = new String("ashok");
+		String st = new String("ashok");
+		System.out.println(str==st);//false
+		System.out.println(str.equals(st));//true
+		
+		/*
+		 * In stringBuffer class .equals() is not overridden for content comparison 
+		 */
+		StringBuffer sb1 = new StringBuffer("ashok");
+		StringBuffer sb2 = new StringBuffer("ashok");
+		System.out.println(sb1==sb2);//false
+		System.out.println(sb1.equals(sb2));//false
 	}
 
 }
