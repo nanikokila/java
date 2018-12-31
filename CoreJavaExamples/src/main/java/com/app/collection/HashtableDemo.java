6a20db9bfbfd057138c50613af6605363ca2fbf2
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-31 11:28:41 +0530

    hashtable

---
+++ CoreJavaExamples/src/main/java/com/app/collection/HashtableDemo.java
@@ -0,0 +1,21 @@
+package com.app.collection;
+
+import java.util.Hashtable;
+
+public class HashtableDemo {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+
+		Hashtable h = new Hashtable();
+		h.put(new Temp(5), "A");
+		h.put(new Temp(2), "B");
+		h.put(new Temp(6), "C");
+		h.put(new Temp(15), "D");
+		h.put(new Temp(23), "E");
+		h.put(new Temp(16), "F");
+		//h.put("Durga",null); //RE: java.lang.NullPointerException
+		System.out.println(h);
+	}
+
+}
