c3cc6352e4806404b610152e8f9e0c747067294e
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-31 12:09:22 +0530

    arrays search

---
+++ CoreJavaExamples/src/main/java/com/app/collection/ArraysSearchDemo.java
@@ -0,0 +1,25 @@
+package com.app.collection;
+
+import java.util.Arrays;
+
+public class ArraysSearchDemo {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+
+		int []a = {10,5,20,11,6};
+		Arrays.sort(a);
+		System.out.println(Arrays.binarySearch(a, 6));
+		System.out.println(Arrays.binarySearch(a, 14));
+		
+		String s[] = {"A","Z","B"};
+		Arrays.sort(s);
+		System.out.println(Arrays.binarySearch(s, "Z"));
+		System.out.println(Arrays.binarySearch(s, "R"));
+		
+		Arrays.sort(s, new MyComparator1());
+		System.out.println(Arrays.binarySearch(s,"Z",new MyComparator1()));
+		System.out.println(Arrays.binarySearch(s,"S",new MyComparator1()));
+	}
+
+}
