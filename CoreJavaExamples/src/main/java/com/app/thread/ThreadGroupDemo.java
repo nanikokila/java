39f2d856d06e5be58d8291f6e908edfe7a732f88
VasariAshokReddy <ashokreddy.va5@gmail.com>
2018-12-31 17:07:32 +0530

    threadgrp added

--- CoreJavaExamples/src/main/java/com/app/thread/ThreadGroupDemo.java
+++ CoreJavaExamples/src/main/java/com/app/thread/ThreadGroupDemo.java
@@ -10,6 +10,15 @@ public class ThreadGroupDemo {
 		System.out.println(tg.getParent().getName());
 		ThreadGroup tg1 = new ThreadGroup(tg, "Chile Group");
 		System.out.println(tg1.getParent().getName());
+		
+		ThreadGroup g = new ThreadGroup("tg");
+		Thread t = new Thread(g, "Thread1");
+		Thread t1 = new Thread(g, "Thread2");
+		g.setMaxPriority(3);
+		Thread t2 = new Thread(g, "Thread3");
+		System.out.println(t.getPriority());
+		System.out.println(t1.getPriority());
+		System.out.println(t2.getPriority());
 	}
 
 }
