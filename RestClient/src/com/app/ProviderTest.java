a8eacb5e7101c68dad5f01cb4e5455cac3670fa0
VasariAshokReddy <ashokreddy.va5@gmail.com>
2019-01-05 20:09:46 +0530

    client example

---
+++ RestClient/src/com/app/ProviderTest.java
@@ -0,0 +1,27 @@
+package com.app;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class ProviderTest {
+
+	public static void main(String[] args) {
+
+		//create empty client object
+		Client c = Client.create();
+		//Define one URL
+		String url = "http://localhost:1010/RestfulWebservices/rest/ashok/ramya";
+		//URL to the client object
+		WebResource wr = c.resource(url);
+		
+		ClientResponse cr = wr.get(ClientResponse.class);
+		//Read entity from client ClientResponse object
+		String s = cr.getEntity(String.class);
+		//print entity message
+		System.out.println(s);
+		System.out.println(cr.getStatus());
+		System.out.println(cr.getStatusInfo());
+	}
+
+}
