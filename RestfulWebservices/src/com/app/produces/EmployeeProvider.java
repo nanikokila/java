ee92a0ada94f7ced3b44369125a8558672b3fe1f
VasariAshokReddy <ashokreddy.va5@gmail.com>
2019-01-08 20:26:04 +0530

    jsonproduces

---
+++ RestfulWebservices/src/com/app/produces/EmployeeProvider.java
@@ -0,0 +1,23 @@
+package com.app.produces;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/aaa")
+public class EmployeeProvider {
+
+	@Path("/rrr")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	//@Produces("application/json")
+	public Employee showMessage()
+	{
+		Employee emp=  new Employee();
+		emp.setEmpId(1);
+		emp.setEmpName("Ashok");
+		emp.setEmpSal(45000.0);
+		return emp;
+	}
+}
