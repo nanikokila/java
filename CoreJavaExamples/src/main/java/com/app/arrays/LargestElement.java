a5d796cb8d2ffff4d648b17399e09239dbf83615
VasariAshokReddy <ashokreddy.va5@gmail.com>
2019-01-10 19:40:17 +0530

    maxelement

---
+++ CoreJavaExamples/src/main/java/com/app/arrays/LargestElement.java
@@ -0,0 +1,20 @@
+package com.app.arrays;
+
+public class LargestElement {
+
+	public static void main(String[] args) {
+
+		int arr[] = new int[] {5,25,56,1,85,78};
+		int max =arr[0];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>max)
+			{
+				max=arr[i];
+			}
+			
+		}
+		System.out.println(max);
+	}
+
+}
