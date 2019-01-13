c994b26aa311ba7c6618b8a19cd95a8dda430b51
VasariAshokReddy <ashokreddy.va5@gmail.com>
2019-01-13 22:07:43 +0530

    first servlet

---
+++ ServletPractice/src/com/app/MyFirstServlet.java
@@ -0,0 +1,35 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+public class MyFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//create write object
+		PrintWriter pw = response.getWriter();
+		//print data
+		pw.println("Hi Ashok welcome to servlet...");
+	}
+
+}
