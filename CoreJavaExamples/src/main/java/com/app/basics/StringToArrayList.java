f6b145aaba6dbf6b700ce2a1888aa19e3d82182c
VasariAshokReddy <ashokreddy.va5@gmail.com>
2019-02-07 21:03:46 +0530

    STAL

---
+++ CoreJavaExamples/src/main/java/com/app/basics/StringToArrayList.java
@@ -0,0 +1,15 @@
+package com.app.basics;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringToArrayList {
+
+	public static void main(String[] args) {
+
+		String[] str = {"ashok","ramya"};
+		List<String> list = Arrays.asList(str);
+		System.out.println("List"+list);
+	}
+
+}
