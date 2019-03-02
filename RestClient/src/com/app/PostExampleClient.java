d205bf53c76d28822082bb6387545f546ccf766c
VasariAshokReddy <ashokreddy.va5@gmail.com>
2019-03-02 19:25:28 +0530

    restpost

---
+++ RestClient/src/com/app/PostExampleClient.java
@@ -0,0 +1,69 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.representation.Form;
+
+public class PostExampleClient {
+
+	
+	 private static final String BASE_URI = "http://localhost:1010/RestfulWebservices";
+	    private static final String PATH_NAME = "/PostExampleService/empInfo";
+	    public static void main(String[] args) throws IOException {
+	        Map<String, String> formDataMap = new HashMap<String, String>();
+	        formDataMap.put("name", "Ashok");
+	        formDataMap.put("age", "25");
+	        ClientResponse res = post(formDataMap);
+	        System.out.println("res status :: "+ res.getStatus());
+	        System.out.println("res :: "+ getResponse(res));      
+	    }
+	    /**
+	     * Post Method
+	     * @param formDataMap
+	     * @return
+	     */
+	    private static ClientResponse post(Map<String, String> formDataMap){
+	        ClientConfig config = new DefaultClientConfig();
+	        Client client = Client.create(config);
+	        WebResource resource = client.resource(BASE_URI);
+	        WebResource nameResource = resource.path("rest").path(PATH_NAME);
+	        Form form = new Form();
+	        Iterator<String> itr = formDataMap.keySet().iterator();
+	        while(itr.hasNext()){
+	            String key = itr.next();
+	            form.add(key, formDataMap.get(key));
+	        }  
+	        ClientResponse res = nameResource.post(ClientResponse.class, form);
+	        System.out.println("Client Response \n" + getClientResponse(nameResource));
+	        return res;
+	    }
+	    /**
+	     * Returns client response
+	     * @param resource
+	     * @return
+	     */
+	    private static String getClientResponse(WebResource resource) {
+	        return resource.getURI().toString();
+	    }
+	    /**
+	     * Returns the response
+	     * @param res
+	     * @return
+	     * @throws IOException
+	     */
+	    private static String getResponse(ClientResponse res) throws IOException {      
+	        InputStream is = res.getEntityInputStream();
+	        byte[] b = new byte[20000];
+	        is.read(b);
+	        return new String(b);
+	    }
+}
